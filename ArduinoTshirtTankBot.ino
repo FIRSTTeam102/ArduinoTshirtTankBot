a695fafecabd16d2326aa5014d735bf33f7a4400
Calum Dingwall <29152895+caburum@users.noreply.github.com>
2022-09-09 16:52:03 +0000

    make it functional programming because classes and servo library just dont seem to like each other +codespaces config

--- ArduinoTshirtTankBot.ino
+++ ArduinoTshirtTankBot.ino
@@ -1,7 +1,9 @@
-#include "SparkPWM.h"
+#include <Servo.h>
 
-SparkPWM tankLeft{5};
-SparkPWM tankRight{6};
+#define PIN_TANK_LEFT 5
+#define PIN_TANK_RIGHT 6
+Servo tankLeft;
+Servo tankRight;
 float tankLeftSpeed = 0.0;
 float tankRightSpeed = 0.0;
 
@@ -17,39 +19,69 @@ float driveSpeed = 0.5; // adjustable with pot
 
 void setup() {
 	Serial.begin(9600);
+	tankLeft.attach(PIN_TANK_LEFT);
+	pinMode(PIN_TANK_LEFT, OUTPUT);
+	tankRight.attach(PIN_TANK_RIGHT);
+	pinMode(PIN_TANK_RIGHT, OUTPUT);
 	pinMode(BTN_LEFT_FWD, INPUT_PULLUP);
 	pinMode(BTN_LEFT_BWD, INPUT_PULLUP);
 	pinMode(BTN_RIGHT_FWD, INPUT_PULLUP);
 	pinMode(BTN_RIGHT_BWD, INPUT_PULLUP);
 }
 
-void updateFloatFromSerial(float* val) {
+void updateFloatFromSerial(float *val) {
 	if (Serial.available() > 0) {
 		String input = Serial.readStringUntil('\n');
 		*val = (float)atof(input.c_str());
 	}
 }
 
-// handles reading and setting the speed of the motor from 3-val rocker
-void updateMotorSpeed(SparkPWM* motor, float* spd, byte btnFwd, byte btnBwd) {
-	if (!digitalRead(btnFwd)) *spd = driveSpeed;
-	else if (!digitalRead(btnBwd)) *spd = -1.0 * driveSpeed;
-	else *spd = 0.0;
+/*
+ * Writes the speed to the motor
+ * Pulse: 1ms - 1.5ms - 2ms
+ * which means full reverse - stop - full forward and everything in between
+ * The servo library handles values in the 20ms period with writeMicroseconds
+ */
+// from https://github.com/wpilibsuite/allwpilib/blob/main/wpilibc/src/main/native/include/frc/motorcontrol/Spark.h
+#define PWM_MAX_BWD 999
+#define PWM_MIN_BWD 1450
+#define PWM_STOP 1500
+#define PWM_MIN_FWD 1550
+#define PWM_MAX_FWD 2003
+void setSparkSpeed(Servo motor, float speed) {
+	speed = constrain(speed, -1.0, 1.0);
+	int pulse;
+	if (speed < 0) {
+		pulse = (int)(PWM_MIN_BWD - fabs(speed) * (PWM_MIN_BWD - PWM_MAX_BWD));
+	} else if (speed > 0) {
+		pulse = (int)(PWM_MIN_FWD + speed * (PWM_MAX_FWD - PWM_MIN_FWD));
+	} else
+		pulse = PWM_STOP;
+	motor.writeMicroseconds(pulse);
+}
 
-	motor->setSpeed(*spd);
+// handles reading and setting the speed of the motor from 3-val rocker
+void driveMotorSpeed(Servo motor, float *spd, byte btnFwd, byte btnBwd) {
+	if (!digitalRead(btnFwd))
+		*spd = driveSpeed;
+	else if (!digitalRead(btnBwd))
+		*spd = -1.0 * driveSpeed;
+	else
+		*spd = 0.0;
+	setSparkSpeed(motor, *spd);
 };
 
 void loop() {
 	driveSpeed = analogRead(POT_SPEED) * (DRIVE_SPEED_MAX - DRIVE_SPEED_MIN) / 1024.0 + DRIVE_SPEED_MIN;
 
-//	updateMotorSpeed(&tankLeft, &tankLeftSpeed, BTN_LEFT_FWD, BTN_LEFT_BWD);
-//	updateMotorSpeed(&tankRight, &tankRightSpeed, BTN_RIGHT_FWD, BTN_RIGHT_BWD);
+	driveMotorSpeed(tankLeft, &tankLeftSpeed, BTN_LEFT_FWD, BTN_LEFT_BWD);
+	driveMotorSpeed(tankRight, &tankRightSpeed, BTN_RIGHT_FWD, BTN_RIGHT_BWD);
 
-//	updateFloatFromSerial(&tankLeftSpeed);
-	tankLeft.setSpeed(driveSpeed);
+	// updateFloatFromSerial(&tankLeftSpeed);
+	// setSparkSpeed(tankLeft, driveSpeed);
 
-	Serial.print("speed: L"); Serial.print(tankLeftSpeed);
-	Serial.print(" R"); Serial.println(tankRightSpeed);
-
-	delay(15);
+	Serial.print("speed: L");
+	Serial.print(tankLeftSpeed);
+	Serial.print(" R");
+	Serial.println(tankRightSpeed);
 }
